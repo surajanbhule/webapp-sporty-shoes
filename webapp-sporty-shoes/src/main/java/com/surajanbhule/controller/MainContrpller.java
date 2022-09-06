33766f65222398c2090eaf08d882f6c3e27a7da5
surajanbhule <surajanbhule69@gmail.com>
2022-09-06 12:14:32 +0530

    Frontend
    Navbar and Theme added successfully.

---
+++ webapp-sporty-shoes/src/main/java/com/surajanbhule/controller/MainContrpller.java
@@ -0,0 +1,13 @@
+package com.surajanbhule.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainContrpller {
+
+	@RequestMapping("/")
+	public String home() {
+		return "index";
+	}
+}
