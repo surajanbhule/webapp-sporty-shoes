0127a189d9b8c8a395954d3f2aee5f47f3c8a277
surajanbhule <surajanbhule69@gmail.com>
2022-09-06 09:58:19 +0530

    Project setup don successfully.

---
+++ webapp-sporty-shoes/src/test/java/com/surajanbhule/WebappSportyShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surajanbhule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappSportyShoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
